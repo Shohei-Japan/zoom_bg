9fdacf31967947f11f78d8efef9ec9700a21a348
Shohei-Japan <kai.entertainment0304@gmail.com>
2020-04-29 04:35:34 +0900

    add header footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app_title">
+    <div class="flex flex-col items-center">
+      <div class="center">
+        <div class="flex justify-around mb-1">
+          <a
+            class="mr-1"
+            href="https://twitter.com/show60"
+            target="_blank"
+          >
+            <icon-twitter class="fill-current text-blue-500 hover:text-blue-400 cursor-pointer duration-200 inline-block h-6 w-6"/>
+          </a>
+          <a
+            href="https://github.com/Shohei-Japan/self_introduction_bg"
+            target="_blank"
+          >
+            <icon-github class="fill-current text-purple-500 hover:text-purple-400 cursor-pointer duration-200 inline-block h-6 w-6"/>
+          </a>
+        </div>
+        <span class="text-xs">
+          created by show60
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconGithub from '~/components/icons/IconGithub'
+import IconTwitter from '~/components/icons/IconTwitter'
+
+export default {
+  name: 'AppFooter',
+  components: {
+    IconGithub,
+    IconTwitter
+  }
+}
+</script>
---
+++ components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app_title">
+    <div class="flex flex-col items-center">
+      <h1 class="text-xl md:text-3xl lg:text-4xl">Zoom 自己紹介背景メーカー</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconGithub from '~/components/icons/IconGithub'
+import IconTwitter from '~/components/icons/IconTwitter'
+
+export default {
+  name: 'AppHeader',
+  components: {
+    IconGithub,
+    IconTwitter
+  }
+}
+</script>
